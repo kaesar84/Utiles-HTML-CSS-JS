f0e8a3438a262f788ee826d7e8eb515d27eac1f2
Cesar <kaesar84@icloud.com>
2023-03-28 11:58:57 +0200

    formulario
    Formulario acabado valida campos con js

--- modal-formulari/scripts.js
+++ modal-formulari/scripts.js
@@ -1,6 +1,3 @@
-
-
-
 /*
 function test(){
   alert("Bienvenido a la web");
@@ -8,30 +5,40 @@ function test(){
 }
 */
 
-
-
-
-contactarAbrir= ()=>{
-  document.getElementById("modalFormulario").style.display="flex";
+contactarAbrir = () => {
+  document.getElementById("modalFormulario").style.display = "flex";
   /*document.getElementById("container-formulario").style.display="none";*/
-  document.documentElement.style.overflow="hidden";
-var nombre = document.getElementById("formNombre").value;
-var apellidos = document.getElementById("formApellidos").value;
-var telefono = document.getElementById("formTel").value;
-var email = document.getElementById("formEmail").value;
+  document.documentElement.style.overflow = "hidden";
+  var nombre = document.getElementById("formNombre").value;
+  var apellidos = document.getElementById("formApellidos").value;
+  var telefono = document.getElementById("formTel").value;
+  var email = document.getElementById("formEmail").value;
 
-var mensaje;
+  var mensaje;
 
-mensaje = "Hola " + nombre + " te contactaremos muy pronto";
+  (function formCheck() {
+    if (!document.getElementById("formNombre").checkValidity()) {
+      mensaje = "Introduce un nombre correcto";
+      document.getElementById("mensajeFormulario").innerHTML = mensaje;
+    } else if (!document.getElementById("formApellidos").checkValidity()) {
+      mensaje = "Introduce un apellido correcto";
+      document.getElementById("mensajeFormulario").innerHTML = mensaje;
+    } else if (!document.getElementById("formTel").checkValidity()) {
+      mensaje = "Introduce un télefono correcto";
+      document.getElementById("mensajeFormulario").innerHTML = mensaje;
+    } else if (!document.getElementById("formEmail").checkValidity()) {
+      mensaje = "Introduce e-mail correcto";
+      document.getElementById("mensajeFormulario").innerHTML = mensaje;
+    } else {
+      mensaje =
+        "Hola " + nombre + "<br>Contactaremos en tu teléfono " + telefono;
+      document.getElementById("mensajeFormulario").innerHTML = mensaje;
+    }
+  })();
 
-
-
-
-
-}
-
-contactarCerrar= ()=>{
-  document.getElementById("modalFormulario").style.display="none";
-  /*document.getElementById("container-formulario").style.display="flex";*/
-  document.documentElement.style.overflow="auto";
-}
+  contactarCerrar = () => {
+    document.getElementById("modalFormulario").style.display = "none";
+    /*document.getElementById("container-formulario").style.display="flex";*/
+    document.documentElement.style.overflow = "auto";
+  };
+};
