75bac18bec3cc3945404af67be695d241adb7dd3
Cesar <kaesar84@icloud.com>
2023-04-08 20:13:55 +0200

    UPDATE

--- filtro2/scripts.js
+++ filtro2/scripts.js
@@ -1,15 +1,14 @@
 
 function filterImages(category) {
-    const imagenesFiltro = document.getElementsByClassName('container-filtro')[0].getElementsByClassName('imgfiltro');
+    const imagenesFiltro = document.getElementsByClassName('imgfiltro');
     
     for (let i = 0; i < imagenesFiltro.length; i++) {
       const imagenesFiltroS = imagenesFiltro[i];
      
-      
       if (category === 'todos') {
-        imagenesFiltroS.style.display = 'block';
+        imagenesFiltroS.style.display = 'flex';
       } else if (imagenesFiltroS.classList.contains(category)) {
-        imagenesFiltroS.style.display = 'block';
+        imagenesFiltroS.style.display = 'flex';
       } else {
         imagenesFiltroS.style.display = 'none';
       }
